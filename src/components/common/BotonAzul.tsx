04e74253880cf238e4b02515e8ed78b143f1e2b5
exam <example@hotmail.com>
2025-04-08 20:34:23 +0200

    responsive

--- src/components/common/BotonAzul.tsx
+++ src/components/common/BotonAzul.tsx
@@ -1,6 +1,6 @@
 export const BotonAzul = () => {
   return (
-    <div className="flex w-4xl mx-auto flex-col gap-4">
+    <div className="flex w-full mx-auto flex-col gap-4">
       <button className="bg-blue-500 cursor-pointer hover:bg-blue-600 transition-all duration-300 text-white p-2 rounded-md">
         Este es un botón azul estilizado con clases por defecto de tailwind
       </button>
