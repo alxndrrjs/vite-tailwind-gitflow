b5a1dba492fc3e5fdef7136c944eb786a4d0965d
exam <example@hotmail.com>
2025-04-08 18:56:09 +0200

    holamundo app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BotonAzul } from './components/common/BotonAzul'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { BotonAzul } from './components/common/BotonAzul';
 
 function App() {
-
   return (
     <>
+      <h1>Hola mundo header</h1>
       <BotonAzul />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
